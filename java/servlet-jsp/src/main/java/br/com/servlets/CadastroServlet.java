4656764e86d80e4017ebf77907c49f038ea8bea1
GustavoCostaAM <gustavo.costa.m949@gamil.com>
2025-06-06 14:33:43 -0300

    exemplo de servlet criado em cadastro

--- java/servlet-jsp/src/main/java/br/com/servlets/CadastroServlet.java
+++ java/servlet-jsp/src/main/java/br/com/servlets/CadastroServlet.java
@@ -1,16 +1,29 @@
 package br.com.servlets;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet("/Register")
 public class CadastroServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+        //pegando os parametros
+        String nome = req.getParameter("nome");
+        String idade = req.getParameter("idade");
+
+        //saida dos dados
+        resp.setContentType("text/html");
+        resp.getWriter().println("<html>");
+        resp.getWriter().println("<body>");
+        resp.getWriter().println("<p>Nome: " + nome + "</p>");
+        resp.getWriter().println("<p>Idade: " + idade + "</p>");
+        resp.getWriter().println("</html>");
+        resp.getWriter().println("</body>");
     }
 }
